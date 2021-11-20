010dd067a5e4646d7691a2867b7ac26d396e35b5
chaos4ros2 <liuchao46642@yahoo.co.jp>
2021-11-20 23:56:56 +0900

    Add message display UI

--- public/js/client.js
+++ public/js/client.js
@@ -1,9 +1,14 @@
 var socket = io();
 
-var main_area = document.getElementById('main_area');
-
 socket.on("topic_list", list_topic);
 
+// an area to stock messages
+var message_store = document.getElementById('message_store');
+
+// winboxn event obj
+var winbox_obj = {};
+
+// parent element of topic button 
 var topics_ul = document.getElementById('topics_ul');
 
 async function list_topic(topic_list) {
@@ -15,16 +20,16 @@ async function list_topic(topic_list) {
       // create topic display area and append it to main area
       if (!document.getElementById(topic_name)) {
         var display_area = create_area (topic_name);
-        main_area.appendChild(display_area);
+        message_store.appendChild(display_area);
       } else {
         var display_area = document.getElementById(topic_name);
       }
 
       // Todo1: Add automatic message type detection.
-      // Todo2: Add message display UI. 
       if (arg.data) display_area.innerHTML = arg.data;
       else if (arg.x) display_area.innerHTML = arg.x;
       else console.log(arg);
+      show_box (topic_name);
     });
   }
 
@@ -39,6 +44,7 @@ async function list_topic(topic_list) {
      </a>
   </li>`).join('');
   topics_ul.innerHTML = `${list}`;
+  
 }
 
 /**
@@ -52,4 +58,16 @@ function create_area (topic_name) {
   display_div.id = topic_name;
   display_div.className = 'display_div';
   return display_div;
+}
+
+/**
+ * create a winbox for display and clone display_div to it 
+ * 
+ * @param {string} topic_name topic name
+ * @returns 
+ */
+ function show_box (topic_name) {
+   if (!winbox_obj[topic_name]) winbox_obj[topic_name] = new WinBox(topic_name);
+   if (document.getElementById(topic_name))
+       winbox_obj[topic_name].mount(document.getElementById(topic_name).cloneNode(true));
 }
